91e9982d45f6d0ce2a2677a49b347340b64bfea8
okhwaja <osman.khwaja@gmail.com>
2014-03-29 15:09:50 -0400

    init

---
+++ Extra Man/Extra Man/EMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EMAppDelegate.h
+//  Extra Man
+//
+//  Created by Osman Khwaja on 3/29/14.
+//  Copyright (c) 2014 Osman Khwaja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Extra Man/Extra Man/EMViewController.h
@@ -0,0 +1,13 @@
+//
+//  EMViewController.h
+//  Extra Man
+//
+//  Created by Osman Khwaja on 3/29/14.
+//  Copyright (c) 2014 Osman Khwaja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EMViewController : UIViewController
+
+@end
